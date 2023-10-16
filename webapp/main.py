03812c9c68b4cb038f7dd1795d539172cbfd5046
Miguefi <114520337+Miguefi@users.noreply.github.com>
2023-10-16 16:36:55 +0000

    API

--- webapp/main.py
+++ webapp/main.py
@@ -13,9 +13,30 @@ static_path = join(current_dir, "static")
 app = FastAPI()
 app.mount("/ui", StaticFiles(directory=static_path), name="ui")
 
+# Define a pydantic model called Body
+# that will be used to validate the request body.
+# The length field is optional and will default to 20 if not provided.
+# The body of the request will be validated against this model
+# before the generate function is called. If the body is invalid,
+# FastAPI will return a 422 Unprocessable Entity response.
+
 
 class Body(BaseModel):
     length: Union[int, None] = 20
+
+
+# Define another Pydantic model called Text that accepts text as a string.
+
+class Text(BaseModel):
+    text: str
+
+# Create a FastAPI endpoint that accepts a POST request with a JSON body
+# containing a single field called "text" and returns a checksum of the text
+
+
+@app.post('/checksum')
+def checksum(text: Text):
+    return {'checksum': hash(text.text)}
 
 
 @app.get('/')
@@ -27,7 +48,8 @@ def root():
 @app.post('/generate')
 def generate(body: Body):
     """
-    Generate a pseudo-random token ID of twenty characters by default. Example POST request body:
+    Generate a pseudo-random token ID of twenty characters by default.
+    Example POST request body:
 
     {
         "length": 20
